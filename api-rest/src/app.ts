53f3d411a84d7457721b768707432b471ae568c0
OpoliNinja <jairo@opoli.ninja>
2023-03-30 09:40:26 -0600

    feat: create server

---
+++ api-rest/src/app.ts
@@ -0,0 +1,24 @@
+import {config} from "dotenv"
+import express, {Express, Request, Response, NextFunction} from "express"
+import helmet from "helmet"
+
+// Initializations
+config()
+const app: Express = express()
+
+// Settings
+app.set("port", process.env.PORT || 5000);
+
+// Middlewares
+app.use(helmet());
+app.use(express.json());
+
+// Routes
+// TODO
+
+// Error Handler
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+	return res.status(500).json({});
+});
+
+export default app;
