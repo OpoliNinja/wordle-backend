cc29b185ca8804497cd497f2ee9f45f6a4ebc892
OpoliNinja <jairo@opoli.ninja>
2023-03-30 10:00:26 -0600

    style: lint

--- api-rest/src/app.ts
+++ api-rest/src/app.ts
@@ -1,5 +1,5 @@
-import {config} from "dotenv"
-import express, {Express, Request, Response, NextFunction} from "express"
+import { config } from "dotenv"
+import express, { Express, Request, Response, NextFunction } from "express"
 import helmet from "helmet"
 import router from "../routes"
 
@@ -8,18 +8,18 @@ config()
 const app: Express = express()
 
 // Settings
-app.set("port", process.env.PORT || 5000);
+app.set("port", process.env.PORT || 5000)
 
 // Middlewares
-app.use(helmet());
-app.use(express.json());
+app.use(helmet())
+app.use(express.json())
 
 // Routes
 app.use(router)
 
 // Error Handler
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-	return res.status(500).json({});
-});
+	return res.status(500).json({})
+})
 
-export default app;
+export default app
